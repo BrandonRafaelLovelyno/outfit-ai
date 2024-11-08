a0768166132948206340ebda09ae0a71ac928bf3
Brandon Rafael Lovelyno <brandonrafael80@gmail.com>
2024-11-08 21:09:33 +0700

    initialize color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,29 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          dark: "#021526",
+          normal: "#03346E",
+          light: "#6EACDA",
+        },
+        secondary: "#F2F4F7",
+        acent: {
+          orange: {
+            dark: "#DE8F5F",
+            normal: "#FFB26F",
+            light: "#FFB38E",
+          },
+          purple: {
+            dark: "#2E073F",
+            normal: "#7A1CAC",
+            light: "#AD49E1",
+          },
+          teal: {
+            dark: "#243642",
+            normal: "#387478",
+            light: "#629584",
+          },
+        },
       },
     },
   },
